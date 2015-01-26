92f8f3352496e9b0717ae494d088fbdfc5ccbc14
naubergois <naubergois@gmail.com>
2015-01-26 10:21:36 -0300

    New Version with jmeter

--- Agent/src/main/java/br/nauber/iadapter/function/JMeterThread.java
+++ Agent/src/main/java/br/nauber/iadapter/function/JMeterThread.java
@@ -1,23 +1,24 @@
 package br.nauber.iadapter.function;
 
 import java.io.FileInputStream;
+import java.text.SimpleDateFormat;
 import java.util.List;
 
 import org.apache.jmeter.engine.StandardJMeterEngine;
 import org.apache.jmeter.save.SaveService;
 import org.apache.jmeter.testelement.TestElement;
 import org.apache.jmeter.testelement.TestPlan;
-
 import org.apache.jmeter.util.JMeterUtils;
+import org.apache.jmeter.visualizers.SimpleDataWriter;
 import org.apache.jmeter.visualizers.ViewResultsFullVisualizer;
 import org.apache.jorphan.collections.HashTree;
 
 public class JMeterThread extends Thread {
-	
+
 	private long time;
-	
-	public JMeterThread(String script){
-		this.scriptJMeter=script;
+
+	public JMeterThread(String script) {
+		this.scriptJMeter = script;
 	}
 
 	private String scriptJMeter;
@@ -61,9 +62,9 @@ public class JMeterThread extends Thread {
 			// Run JMeter Test
 			jmeter.configure(testPlanTree);
 			jmeter.run();
-			
+
 			System.out.println(testPlanTree);
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 
@@ -71,14 +72,14 @@ public class JMeterThread extends Thread {
 		// TODO Auto-generated method stub
 		super.run();
 	}
-	
-	public static void main(String args[]){
+
+	public static void main(String args[]) {
 		try {
 			// JMeter Engine
 			StandardJMeterEngine jmeter = new StandardJMeterEngine();
 
 			String workingDir = System.getProperty("user.dir");
-			
+
 			System.out.println(workingDir);
 
 			// get location of workspace (java.io.File)
@@ -86,18 +87,18 @@ public class JMeterThread extends Thread {
 			// Initialize Properties, logging, locale, etc.
 			JMeterUtils.loadJMeterProperties(workingDir
 					+ "/apache-jmeter-2.12/bin/jmeter.properties");
-			
-			
+
 			JMeterUtils.setJMeterHome(workingDir + "/apache-jmeter-2.12/");
 			JMeterUtils.initLogging();// you can comment this line out to see
 										// extra log messages of i.e. DEBUG
-										// level
 			
-			JMeterListener listener=new JMeterListener();
+			// level
+			JMeterUtils.setProperty("Gthreads", "2");
+
 			JMeterUtils.initLocale();
 
-			//JMeterUtils.loadProperties(workingDir
-				//	+ "/apache-jmeter-2.12/bin/saveservice.properties");
+			// JMeterUtils.loadProperties(workingDir
+			// + "/apache-jmeter-2.12/bin/saveservice.properties");
 			// Initialize JMeter SaveService
 			SaveService.loadProperties();
 
@@ -105,33 +106,34 @@ public class JMeterThread extends Thread {
 			FileInputStream in = new FileInputStream(workingDir
 					+ "/apache-jmeter-2.12/script1.jmx");
 			HashTree testPlanTree = SaveService.loadTree(in);
-Object[] objects=testPlanTree.getArray();
+			Object[] objects = testPlanTree.getArray();
+
+			TestPlan plan = (TestPlan) objects[0];
 			
-			TestPlan plan=(TestPlan) objects[0];
-			plan.addTestElement(listener);
-			testPlanTree.add("monitor", listener);
+			SimpleDataWriter writer=new SimpleDataWriter();
+			writer.setFile(workingDir+"/teste.csv");
+			
+
+			testPlanTree.add("monitor", writer);
 			in.close();
 
 			// Run JMeter Test
+			
 			jmeter.configure(testPlanTree);
+			
 			jmeter.run();
-			
+
 			System.out.println(testPlanTree);
-		
-		
-			
-			
-			
+
 			for (Object object : objects) {
-				System.out.println(object);	
+				System.out.println(object);
 			}
-			
-			
+
 		} catch (Exception e) {
 			e.printStackTrace();
 
 		}
-		
+
 	}
 
 }
